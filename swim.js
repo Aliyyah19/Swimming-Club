50a4a691e408f7fb15a3f71e8b2d34da0aabe5ac
Aliyyah19 <aliyyahnazihah3@gmail.com>
2025-01-22 02:11:15 +0800

    Added togglenavbar

--- swim.js
+++ swim.js
@@ -7,5 +7,10 @@ function moveSlide(direction) {
     slides.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 
-
 setInterval(() => moveSlide(1), 5000); // Change slide every 5 seconds
+
+// Hamburger Menu
+function toggleNavbar() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+}
